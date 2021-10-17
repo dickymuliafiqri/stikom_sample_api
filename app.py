5cd0e4d1beb9ffa434349663259926ce645fa3c7
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2021-10-17 14:33:59 +0700

    Add error handler and correct response code

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods = ["POST", "GET"])
 def root():
     if request.method == "GET":
         return jsonify({
@@ -34,7 +34,7 @@ def root():
             return jsonify({
                 "error": True,
                 "message": "Please fill all required form!"
-            })
+            }), 400
 
         db = json.load(open("./db/db.json"))
 
@@ -44,21 +44,23 @@ def root():
             return jsonify({
                 "error": True,
                 "message": "User ID not found!"
-            })
+            }), 401
         
         if user["pass"] != user_pass:
             return jsonify({
                 "error": True,
                 "message": "Password incorrect!"
-            })
+            }), 401
 
         return jsonify(user)
 
-    else:
-        return jsonify({
-            "error": True,
-            "message": "only post and get method allowed!"
-        })
+@app.errorhandler(404)
+def not_found(error):
+    print(error)
+    return jsonify({
+        "error": True,
+        "message": "404 page not found!"
+    }), 404
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=False)
